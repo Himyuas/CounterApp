9e145890744c9b5a509cf1c8cd183dec6f60fbca
Himanshu <46562206+Himyuas@users.noreply.github.com>
2023-06-13 14:18:22 +0530

    button name change wrong

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export const Button = (props) => {
   return (
-    <button onClick={() =>
-      props.sign === "Increment Counter" ? props.updateCount(1) : props.updateCount(-1)} >
+    <button
+      onClick={() =>
+        props.sign === "Incremet Counter"
+          ? props.updateCount(1)
+          : props.updateCount(-1)
+      }
+    >
       {props.sign}
     </button>
-
   );
-}
-
-
+};
